e36a67753fcaa5676199742ce14b764bc9be58bf
Hasan Jamal <hasanjamalabusharekh@gmail.com>
2025-03-03 15:18:04 +0200

    first commit

---
+++ js/navbar-pages.js
@@ -0,0 +1,15 @@
+function toggleSidenav() {
+  document.body.classList.toggle("sidenav-active");
+}
+function closeSidenav() {
+  document.body.classList.remove();
+}
+// const interval = setInterval(toggleSidenav, 1500);
+document.addEventListener("mousemove", removeInterval);
+document.addEventListener("click", removeInterval);
+
+function removeInterval() {
+  document.removeEventListener("mousemove", removeInterval);
+  document.removeEventListener("click", removeInterval);
+}
+// //////////////////////////////////////////////////////////////////////////////////////
